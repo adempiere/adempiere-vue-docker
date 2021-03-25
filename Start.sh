a5517cf2b16556a0da81565cee41ec1b5ee88c60
Raul Muñoz <rmunoz@erpcya.com>
2021-03-25 11:58:36 -0400

    Add default API for testing project (#16)

--- Start.sh
+++ Start.sh
@@ -2,6 +2,6 @@
 
 cd /usr/share/nginx/html/static/js
 # Replace
-find -name '*.js' -exec sed -i "s/http:\/\/localhost:8085/$API_URL/g" {} \;
+find -name '*.js' -exec sed -i "s/https:\/\/api.erpya.com/$API_URL/g" {} \;
 # Start nginx
 exec nginx;
